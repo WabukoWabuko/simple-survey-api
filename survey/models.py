be7f4ca3cf07f9b627973ff6c73051e7fd0f4eba
WabukoWabuko <basilwabbs@gmail.com>
2025-04-07 17:08:50 +0300

    Added Django models and ran initial migrations for SQLite3

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    type = models.CharField(max_length=20)  # short_text, email, long_text, choice, file
+    required = models.CharField(max_length=3)  # yes, no
+    text = models.TextField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    value = models.CharField(max_length=50)
+    multiple = models.CharField(max_length=3)  # yes, no
+
+    def __str__(self):
+        return self.value
+
+class Response(models.Model):
+    full_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+    description = models.TextField()
+    gender = models.CharField(max_length=20)
+    programming_stack = models.TextField()  # Comma-separated values
+    date_responded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.full_name} - {self.email_address}"
+
+class Certificate(models.Model):
+    response = models.ForeignKey(Response, on_delete=models.CASCADE)
+    file_path = models.CharField(max_length=200)  # Path to file
+
+    def __str__(self):
+        return f"Cert for {self.response.full_name}"
