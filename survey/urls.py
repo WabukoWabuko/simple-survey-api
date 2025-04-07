b74c70e5a86a826ba48487261c6e679c0f2343ce
WabukoWabuko <basilwabbs@gmail.com>
2025-04-07 20:44:22 +0300

    Fixed 500 error on /api/questions/ and 405 error on /api/questions/responses/ by correcting view and URL config

--- survey/urls.py
+++ survey/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('questions/', views.get_questions, name='get_questions'),
-    path('questions/responses/', views.submit_response, name='submit_response'),
-    path('questions/responses/', views.get_responses, name='get_responses'),
+    path('questions/responses/', views.handle_responses, name='handle_responses'),  # Handles both PUT and GET
     path('questions/responses/certificates/<int:id>/', views.download_certificate, name='download_certificate'),
 ]
--- survey/views.py
+++ survey/views.py
@@ -1,5 +1,5 @@
 from rest_framework.decorators import api_view
-from rest_framework.response import Response  
+from rest_framework.response import Response
 from rest_framework import status
 from django.http import FileResponse
 from .models import Question, Response, Certificate
@@ -11,49 +11,48 @@ import os
 def get_questions(request):
     questions = Question.objects.all()
     serializer = QuestionSerializer(questions, many=True)
-    return Response(serializer.data)  
+    return Response(serializer.data)
 
-# PUT /api/questions/responses
-@api_view(['PUT'])
-def submit_response(request):
-    serializer = ResponseSerializer(data=request.data)
-    if serializer.is_valid():
-        response = serializer.save()
-        if request.FILES:
-            for file in request.FILES.getlist('certificates'):
-                cert = Certificate(response=response, file_path=file.name)
-                cert.save()
-                with open(f'media/{file.name}', 'wb+') as destination:
-                    for chunk in file.chunks():
-                        destination.write(chunk)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# PUT and GET /api/questions/responses
+@api_view(['PUT', 'GET'])
+def handle_responses(request):
+    if request.method == 'PUT':
+        serializer = ResponseSerializer(data=request.data)
+        if serializer.is_valid():
+            response = serializer.save()
+            if request.FILES:
+                for file in request.FILES.getlist('certificates'):
+                    cert = Certificate(response=response, file_path=file.name)
+                    cert.save()
+                    with open(f'media/{file.name}', 'wb+') as destination:
+                        for chunk in file.chunks():
+                            destination.write(chunk)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'GET':
+        page = int(request.GET.get('page', 1))
+        page_size = 10
+        email_filter = request.GET.get('email_address', None)
 
-# GET /api/questions/responses
-@api_view(['GET'])
-def get_responses(request):
-    page = int(request.GET.get('page', 1))
-    page_size = 10
-    email_filter = request.GET.get('email_address', None)
+        responses = Response.objects.all()
+        if email_filter:
+            responses = responses.filter(email_address=email_filter)
 
-    responses = Response.objects.all()
-    if email_filter:
-        responses = responses.filter(email_address=email_filter)
+        total_count = responses.count()
+        start = (page - 1) * page_size
+        end = start + page_size
+        paginated_responses = responses[start:end]
 
-    total_count = responses.count()
-    start = (page - 1) * page_size
-    end = start + page_size
-    paginated_responses = responses[start:end]
-
-    serializer = ResponseSerializer(paginated_responses, many=True)
-    response_data = {
-        'current_page': page,
-        'last_page': (total_count + page_size - 1) // page_size,
-        'page_size': page_size,
-        'total_count': total_count,
-        'question_responses': serializer.data
-    }
-    return Response(response_data)
+        serializer = ResponseSerializer(paginated_responses, many=True)
+        response_data = {
+            'current_page': page,
+            'last_page': (total_count + page_size - 1) // page_size,
+            'page_size': page_size,
+            'total_count': total_count,
+            'question_responses': serializer.data
+        }
+        return Response(response_data)
 
 # GET /api/questions/responses/certificates/{id}
 @api_view(['GET'])
